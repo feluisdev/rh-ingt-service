07ab861813bc087bac343e4447f30e5825bf2495
feluisdev <feluis.dev@gmail.com>
2025-07-17 12:27:12 -0100

    add minion logic to save document

---
+++ src/main/java/cv/igrp/RH_Service/shared/application/dto/FileResponseDTO.java
@@ -0,0 +1,29 @@
+/* THIS FILE WAS GENERATED AUTOMATICALLY BY iGRP STUDIO. */
+/* DO NOT MODIFY IT BECAUSE IT COULD BE REWRITTEN AT ANY TIME. */
+
+package cv.igrp.RH_Service.shared.application.dto;
+
+import cv.igrp.framework.stereotype.IgrpDTO;
+import jakarta.validation.*;
+import jakarta.validation.constraints.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
+
+@IgrpDTO
+public class FileResponseDTO {
+
+  
+  
+  private String displayName ;
+  
+  
+  private String fileId ;
+
+}
