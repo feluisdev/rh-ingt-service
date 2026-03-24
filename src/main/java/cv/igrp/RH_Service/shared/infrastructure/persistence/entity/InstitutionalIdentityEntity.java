d897d64ba9cb32a2c414d0a046e5f5a3826050f5
feluisdev <feluis.dev@gmail.com>
2026-03-24 19:01:58 -0100

    feat: add InstitutionalIdentity entity, repository, and supporting modules to the `shared` package

---
+++ src/main/java/cv/igrp/RH_Service/shared/infrastructure/persistence/entity/InstitutionalIdentityEntity.java
@@ -0,0 +1,56 @@
+/* THIS FILE WAS GENERATED AUTOMATICALLY BY iGRP STUDIO. */
+/* DO NOT MODIFY IT BECAUSE IT COULD BE REWRITTEN AT ANY TIME */
+
+package cv.igrp.RH_Service.shared.infrastructure.persistence.entity;
+
+import cv.igrp.RH_Service.shared.config.AuditEntity;
+import cv.igrp.framework.stereotype.IgrpEntity;
+import jakarta.persistence.*;
+import lombok.*;
+import java.util.UUID;
+import jakarta.validation.constraints.NotNull;
+
+
+@Getter
+@Setter
+@IgrpEntity
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "t_institutional_identity")
+public class InstitutionalIdentityEntity extends AuditEntity {
+
+    @Id
+    @Column(name = "id", unique = true, nullable = false)
+    private UUID id;
+
+  
+    @NotNull(message = "cycleYear is mandatory")
+    @Column(name="cycle_year", nullable = false)
+    private Integer cycleYear;
+
+  
+    @Lob
+    @Column(name="mission", columnDefinition="TEXT")
+    private String mission;
+
+  
+    @Lob
+    @Column(name="vision", columnDefinition="TEXT")
+    private String vision;
+
+  
+    @Lob
+    @Column(name="values_json", columnDefinition="TEXT")
+    private String valuesJson;
+
+  
+    @Column(name="version_comment")
+    private String versionComment;
+
+  
+    @Column(name="is_active")
+    private boolean isActive;
+
+  
+}
---
+++ src/main/java/cv/igrp/RH_Service/shared/infrastructure/persistence/repository/InstitutionalIdentityEntityRepository.java
@@ -0,0 +1,24 @@
+package cv.igrp.RH_Service.shared.infrastructure.persistence.repository;
+
+import cv.igrp.RH_Service.shared.infrastructure.persistence.entity.InstitutionalIdentityEntity;
+import cv.igrp.RH_Service.shared.domain.exceptions.IgrpResponseStatusException;
+import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.http.HttpStatus;
+import java.util.Optional;
+import java.util.UUID;
+
+
+@Repository
+public interface InstitutionalIdentityEntityRepository extends
+    JpaRepository<InstitutionalIdentityEntity, UUID>,
+    JpaSpecificationExecutor<InstitutionalIdentityEntity>
+{
+
+      default InstitutionalIdentityEntity findByIdOrThrow(UUID id) {
+          return this.findById(id)
+          .orElseThrow(() -> IgrpResponseStatusException.of(HttpStatus.NOT_FOUND,"InstitutionalIdentityEntity not found for id: " + id));
+      }
+
+}
