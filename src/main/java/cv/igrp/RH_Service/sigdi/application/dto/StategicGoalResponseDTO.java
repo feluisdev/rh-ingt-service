9bcf2c6e9d252564d51b16d4e47ae86674d59dfe
feluisdev <feluis.dev@gmail.com>
2026-03-25 09:38:02 -0100

    feat: add StategicGoalResponseDTO with validation constraints and generated configuration in `sigdi` module

---
+++ src/main/java/cv/igrp/RH_Service/sigdi/application/dto/StategicGoalResponseDTO.java
@@ -0,0 +1,44 @@
+/* THIS FILE WAS GENERATED AUTOMATICALLY BY iGRP STUDIO. */
+/* DO NOT MODIFY IT BECAUSE IT COULD BE REWRITTEN AT ANY TIME */
+
+package cv.igrp.RH_Service.sigdi.application.dto;
+
+import cv.igrp.framework.stereotype.IgrpDTO;
+import jakarta.validation.*;
+import jakarta.validation.constraints.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+import java.math.BigDecimal;
+import java.util.UUID;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
+
+@IgrpDTO
+public class StategicGoalResponseDTO  {
+
+  
+  
+  private UUID id ;
+  @Size(min = 5, message = "The field length <title> must be at least 5 characters")
+	@Size(max = 100, message = "The field length <title> cannot be more than 100 characters")
+  
+  private String title ;
+  @NotBlank(message = "The field <perspective> is required")
+  
+  private String perspective ;
+  @Size(min = 1, message = "The field length <description> must be at least 1 characters")
+	@Size(max = 500, message = "The field length <description> cannot be more than 500 characters")
+  
+  private String description ;
+  
+  
+  private BigDecimal weight ;
+  
+  
+  private String status ;
+
+}
