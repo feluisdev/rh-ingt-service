80d37aeca6495133dc580dd1c4eed6630d3b23fd
feluisdev <feluis.dev@gmail.com>
2026-03-25 12:00:49 -0100

    feat: add `SimulationResultsEntity` and `SimulationScenariosEntity` models with corresponding repositories in `shared` module

---
+++ src/main/java/cv/igrp/RH_Service/shared/infrastructure/persistence/entity/SimulationScenariosEntity.java
@@ -0,0 +1,48 @@
+/* THIS FILE WAS GENERATED AUTOMATICALLY BY iGRP STUDIO. */
+/* DO NOT MODIFY IT BECAUSE IT COULD BE REWRITTEN AT ANY TIME */
+
+package cv.igrp.RH_Service.shared.infrastructure.persistence.entity;
+
+import cv.igrp.RH_Service.shared.config.AuditEntity;
+import cv.igrp.framework.stereotype.IgrpEntity;
+import jakarta.persistence.*;
+import lombok.*;
+import java.util.UUID;
+import jakarta.validation.constraints.NotBlank;
+import java.util.List;
+import java.util.ArrayList;
+
+
+@Getter
+@Setter
+@IgrpEntity
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "t_simulation_scenarios")
+public class SimulationScenariosEntity extends AuditEntity {
+
+    @Id
+    @Column(name = "id", unique = true, nullable = false)
+    private UUID id;
+
+  
+    @NotBlank(message = "name is mandatory")
+    @Column(name="name", nullable = false)
+    private String name;
+
+  
+    @Lob
+    @Column(name="parameters", columnDefinition="TEXT")
+    private String parameters;
+
+  
+    @Column(name="status")
+    private String status;
+
+  
+
+
+  @OneToMany(mappedBy = "scenarioId", fetch = FetchType.LAZY)
+private List<SimulationResultsEntity> simulationResults = new ArrayList<>();
+}
