4f8d32178a06b76f69996045e86a2edd8ad2d2b6
feluisdev <feluis.dev@gmail.com>
2026-03-27 11:51:37 -0100

    feat: implement budget validation in `ValidateBudgetCommandHandler` using `EconomicClassifierPort` and enhanced exception handling

--- src/main/java/cv/igrp/RH_Service/sigdi/application/commands/ValidateBudgetCommandHandler.java
+++ src/main/java/cv/igrp/RH_Service/sigdi/application/commands/ValidateBudgetCommandHandler.java
@@ -1,7 +1,11 @@
 package cv.igrp.RH_Service.sigdi.application.commands;
 
+import cv.igrp.RH_Service.shared.domain.exceptions.IgrpResponseStatusException;
+import cv.igrp.RH_Service.sigdi.application.dto.BudgetInfoDTO;
+import cv.igrp.RH_Service.sigdi.application.port.EconomicClassifierPort;
 import cv.igrp.framework.core.domain.CommandHandler;
 import cv.igrp.framework.stereotype.IgrpCommandHandler;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.slf4j.Logger;
@@ -14,8 +18,11 @@ public class ValidateBudgetCommandHandler implements CommandHandler<ValidateBudg
 
    private static final Logger LOGGER = LoggerFactory.getLogger(ValidateBudgetCommandHandler.class);
 
-   public ValidateBudgetCommandHandler() {
+  private final EconomicClassifierPort economicClassifierPort;
 
+   public ValidateBudgetCommandHandler(EconomicClassifierPort economicClassifierPort) {
+
+     this.economicClassifierPort = economicClassifierPort;
    }
 
    @IgrpCommandHandler
@@ -23,8 +30,22 @@ public class ValidateBudgetCommandHandler implements CommandHandler<ValidateBudg
 
       LOGGER.debug("ValidateBudgetCommand : {}", command);
 
-      // TODO: Implement the command handling logic here
-      return null;
+     var request = command.getBudgetvalidationrequest();
+
+     var economicClassifier = request.getEconomicClassifier();
+
+     BudgetInfoDTO budgetInfo = economicClassifierPort.getBudget(economicClassifier);
+
+     if (request.getBudgetEstimated().compareTo(budgetInfo.availableBudget()) > 0) {
+       throw IgrpResponseStatusException.of(
+           HttpStatus.UNPROCESSABLE_ENTITY,
+           "Budget limit exceeded for this classifier. Available: " + budgetInfo.availableBudget()
+       );
+     }
+     return ResponseEntity.ok(Map.of(
+         "status", "success",
+         "availableBudget", budgetInfo.availableBudget())
+     );
    }
 
 }
