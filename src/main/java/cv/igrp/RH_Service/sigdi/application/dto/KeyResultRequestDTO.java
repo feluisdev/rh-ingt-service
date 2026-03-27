0d51d2b2efb45f018bce7e34dd71f8e0ce46411e
feluisdev <feluis.dev@gmail.com>
2026-03-27 18:25:56 -0100

    add dto's

---
+++ src/main/java/cv/igrp/RH_Service/sigdi/application/dto/KeyResultRequestDTO.java
@@ -0,0 +1,39 @@
+/* THIS FILE WAS GENERATED AUTOMATICALLY BY iGRP STUDIO. */
+/* DO NOT MODIFY IT BECAUSE IT COULD BE REWRITTEN AT ANY TIME */
+
+package cv.igrp.RH_Service.sigdi.application.dto;
+
+import cv.igrp.framework.stereotype.IgrpDTO;
+import jakarta.validation.*;
+import jakarta.validation.constraints.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+import java.math.BigDecimal;
+import java.util.UUID;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
+
+@IgrpDTO
+public class KeyResultRequestDTO  {
+
+  @NotBlank(message = "The field <title> is required")
+  
+  private String title ;
+  @NotNull(message = "The field <targetValue> is required")
+  
+  private BigDecimal targetValue ;
+  
+  
+  private BigDecimal currentValue ;
+  
+  
+  private String metricUnit ;
+  @NotNull(message = "The field <activityId> is required")
+  
+  private UUID activityId ;
+
+}
