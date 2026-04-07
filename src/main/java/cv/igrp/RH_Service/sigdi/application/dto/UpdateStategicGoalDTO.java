7665928551e61204b896d6bf06df510c309635d6
feluisdev <feluis.dev@gmail.com>
2026-04-07 20:40:01 -0100

    add UpdateStategicGoalDTO and corresponding JSON definition

---
+++ src/main/java/cv/igrp/RH_Service/sigdi/application/dto/UpdateStategicGoalDTO.java
@@ -0,0 +1,34 @@
+/* THIS FILE WAS GENERATED AUTOMATICALLY BY iGRP STUDIO. */
+/* DO NOT MODIFY IT BECAUSE IT COULD BE REWRITTEN AT ANY TIME */
+
+package cv.igrp.RH_Service.sigdi.application.dto;
+
+import cv.igrp.framework.stereotype.IgrpDTO;
+import jakarta.validation.*;
+import jakarta.validation.constraints.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+import java.math.BigDecimal;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
+
+@IgrpDTO
+public class UpdateStategicGoalDTO  {
+
+  @Size(min = 5, message = "The field length <title> must be at least 5 characters")
+	@Size(max = 100, message = "The field length <title> cannot be more than 100 characters")
+  
+  private String title ;
+  @Size(min = 1, message = "The field length <description> must be at least 1 characters")
+	@Size(max = 500, message = "The field length <description> cannot be more than 500 characters")
+  
+  private String description ;
+  
+  
+  private BigDecimal weight ;
+
+}
