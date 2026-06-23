0f25e64d26a71a6ac7b7eb3bfe4d1c8a69429790
carlos.m.barros <carlos.m.barros@ingt.gov.cv>
2026-06-23 19:38:18 -0100

    feat: implement flyway migration scripts to initialize and update core HR database tables and audit schemas

---
+++ src/main/resources/db/migration/V17__update_kpi_criteria_fix.sql
@@ -0,0 +1,31 @@
+ALTER TABLE t_key_results DROP COLUMN IF EXISTS criteria_superado;
+ALTER TABLE t_key_results DROP COLUMN IF EXISTS criteria_seguranca;
+ALTER TABLE t_key_results DROP COLUMN IF EXISTS criteria_alcancado;
+ALTER TABLE t_key_results DROP COLUMN IF EXISTS criteria_insuficiente;
+ALTER TABLE t_key_results DROP COLUMN IF EXISTS criteria_seguranca_min;
+ALTER TABLE t_key_results DROP COLUMN IF EXISTS criteria_seguranca_max;
+ALTER TABLE t_key_results DROP COLUMN IF EXISTS criteria_alcancado_min;
+ALTER TABLE t_key_results DROP COLUMN IF EXISTS criteria_alcancado_max;
+
+ALTER TABLE t_key_results ADD COLUMN criteria_superado NUMERIC(19, 2);
+ALTER TABLE t_key_results ADD COLUMN criteria_seguranca_min NUMERIC(19, 2);
+ALTER TABLE t_key_results ADD COLUMN criteria_seguranca_max NUMERIC(19, 2);
+ALTER TABLE t_key_results ADD COLUMN criteria_alcancado_min NUMERIC(19, 2);
+ALTER TABLE t_key_results ADD COLUMN criteria_alcancado_max NUMERIC(19, 2);
+ALTER TABLE t_key_results ADD COLUMN criteria_insuficiente NUMERIC(19, 2);
+
+ALTER TABLE audit_schema.t_key_results_aud DROP COLUMN IF EXISTS criteria_superado;
+ALTER TABLE audit_schema.t_key_results_aud DROP COLUMN IF EXISTS criteria_seguranca;
+ALTER TABLE audit_schema.t_key_results_aud DROP COLUMN IF EXISTS criteria_alcancado;
+ALTER TABLE audit_schema.t_key_results_aud DROP COLUMN IF EXISTS criteria_insuficiente;
+ALTER TABLE audit_schema.t_key_results_aud DROP COLUMN IF EXISTS criteria_seguranca_min;
+ALTER TABLE audit_schema.t_key_results_aud DROP COLUMN IF EXISTS criteria_seguranca_max;
+ALTER TABLE audit_schema.t_key_results_aud DROP COLUMN IF EXISTS criteria_alcancado_min;
+ALTER TABLE audit_schema.t_key_results_aud DROP COLUMN IF EXISTS criteria_alcancado_max;
+
+ALTER TABLE audit_schema.t_key_results_aud ADD COLUMN criteria_superado NUMERIC(19, 2);
+ALTER TABLE audit_schema.t_key_results_aud ADD COLUMN criteria_seguranca_min NUMERIC(19, 2);
+ALTER TABLE audit_schema.t_key_results_aud ADD COLUMN criteria_seguranca_max NUMERIC(19, 2);
+ALTER TABLE audit_schema.t_key_results_aud ADD COLUMN criteria_alcancado_min NUMERIC(19, 2);
+ALTER TABLE audit_schema.t_key_results_aud ADD COLUMN criteria_alcancado_max NUMERIC(19, 2);
+ALTER TABLE audit_schema.t_key_results_aud ADD COLUMN criteria_insuficiente NUMERIC(19, 2);
